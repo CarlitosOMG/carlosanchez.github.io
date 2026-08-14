62a3714afde0701f3b1065862f37bf57ffbbfaa2
Carlos Sánchez <92407304+CarlitosOMG@users.noreply.github.com>
2026-08-14 02:57:34 -0600

    Delete app.js
    no use

--- app.js
+++
@@ -1,5 +0,0 @@
-function copiarTexto() {
-	var texto = document.getElementById('texto').innerText.trim()
-	navigator.clipboard.writeText(texto)
-	alert('Correo electrónico copiado al portapapeles')
-}
